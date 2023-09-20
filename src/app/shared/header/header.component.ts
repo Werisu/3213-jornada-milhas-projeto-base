14b950c460eae25d0556acec751766e2fda4257b
Wellysson Rocha <33838737+Werisu@users.noreply.github.com>
2023-09-20 17:31:31 -0300

    fix: header component

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { UserService } from '../../core/services/user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,4 +9,15 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+  constructor(
+    private userService: UserService,
+    private router: Router
+  ) { }
+
+  user$ = this.userService.retornarUsuario();
+
+  logout() {
+    this.userService.logout();
+    this.router.navigate(['/login']);
+  }
 }
